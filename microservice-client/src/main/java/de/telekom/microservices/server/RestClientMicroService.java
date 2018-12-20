0bf92629c17ccda7c481040923ce5dfca3363bcf
Ravindra Singh <ravindra45.singh@gmail.com>
2018-12-20 16:24:15 +0530

    added load balancer to microservices client

--- microservice-client/src/main/java/de/telekom/microservices/server/RestClientMicroService.java
+++ microservice-client/src/main/java/de/telekom/microservices/server/RestClientMicroService.java
@@ -4,12 +4,13 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import de.telekom.microservices.utils.MicroBean;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.*;
 
 @Component
 public class RestClientMicroService {
@@ -18,17 +19,33 @@ public class RestClientMicroService {
 
     @Autowired
     protected RestTemplate restTemplate;
-
-    public static final String URL = "http://MICROSERVICE-SERVER";
-
+    
     @Autowired
-    DiscoveryClient client;
+	private LoadBalancerClient loadBalancer;
+
+    public static final String URL = "http://MICROSERVICE-SERVER/getMicroResponse/{id}";
+
+    
 
     @Scheduled( fixedRate = 10000 )
     public void getInvoked() {
         LOGGER.info( "0;getMicroResponse method invoked" );
+        
+        String serviceId=null;
+        
+        ServiceInstance serviceInstance=loadBalancer.choose("microservice-server");
+        System.out.println(serviceInstance.getServiceId()+ ","+serviceInstance.getUri());
+        serviceId=serviceInstance.getServiceId().toString();
+        
+        String baseUrl=URL;
+        if(serviceId!=null)
+        	baseUrl  = "http://"+serviceId+"/getMicroResponse/{id}";
+         	
+        
+        LOGGER.info( "0;baseUrl: {}", baseUrl);
+        
         try {
-            MicroBean res = restTemplate.getForObject( URL + "/getMicroResponse/{id}", MicroBean.class, DateTime.now() );
+            MicroBean res = restTemplate.getForObject( baseUrl, MicroBean.class, DateTime.now() );
 
             LOGGER.info( "0;Request received in {}:{}:{} at {} ", res.getServiceId(), res.getHost(),
                     res.getPort(), res.getDate() );
